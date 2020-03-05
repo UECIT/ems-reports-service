ab194bfcb04b9e00f9d9d7ded1ba4f20734ad5b3
Charlie Marcus <charlie.marcus1@nhs.net>
2020-03-05 14:06:07 +0000

    make sure snomed code is valid for referred to service code

--- src/main/java/uk/nhs/cdss/reports/transform/ecds/ReferralsToOtherServicesTransformer.java
+++ src/main/java/uk/nhs/cdss/reports/transform/ecds/ReferralsToOtherServicesTransformer.java
@@ -4,7 +4,9 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 import java.util.Calendar;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.ReferralRequest;
 import org.springframework.stereotype.Component;
 import uk.nhs.nhsia.datastandards.ecds.AttendanceOccurrenceECStructure.ReferralsToOtherServices;
@@ -32,7 +34,7 @@ public class ReferralsToOtherServicesTransformer {
     ReferralsToOtherServices referral = ReferralsToOtherServices.Factory.newInstance();
 
     if (referralRequest.hasServiceRequested()) {
-      referral.setReferredToServiceSnomedCt(referralRequest.getServiceRequestedFirstRep().getCodingFirstRep().getCode());
+      referral.setReferredToServiceSnomedCt(toServiceSnomedCode(referralRequest.getServiceRequestedFirstRep()));
     }
 
     referral.xsetActivityServiceRequestDateEmergencyCare(
@@ -40,6 +42,16 @@ public class ReferralsToOtherServicesTransformer {
     referral.xsetActivityServiceRequestTimeEmergencyCare(
         DateTimeFormatter.formatTime(authoredOn, TimeType.type));
     return referral;
+  }
+
+  private String toServiceSnomedCode(CodeableConcept codeableConcept) {
+    String code = codeableConcept.getCodingFirstRep().getCode();
+    String validSixCharCode = StringUtils.repeat(code, 2);
+
+    if (validSixCharCode.length() < 6) {
+      return "27659004"; // Default to 'Referral to member of Primary Health Care Team'
+    }
+    return validSixCharCode;
   }
 
 }
